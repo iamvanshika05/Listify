b6e83cfe0090b7b5debbebecf825404ca550e594
iamvanshika05 <heartedprincessvanshi@gmail.com>
2023-10-12 23:51:42 +0530

    Update EditTodo.jsx

--- client/src/components/EditTodo.jsx
+++ client/src/components/EditTodo.jsx
@@ -19,7 +19,7 @@ function EditTodo({todo}) {
                 body:JSON.stringify(body)
               })   //we have use backtick here for diplaying value of id
 
-              window.location = "/"  //refreshing the code
+              window.location = "/"  //refreshing the code automatically data udgya
         }catch(err){
             console.log(err.message)
         }
